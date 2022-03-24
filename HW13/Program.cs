a448bd6973c3de628b2bec9ddd1f483657ec2789
Igor <pilingator86@gmail.com>
2022-03-24 21:40:21 +0700

    Программа считает сумму цифр введенного числа

---
+++ HW13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+int i = 0;
+int sum = 0;
+while(N > 0)
+{
+    sum = sum + (N % 10);
+    N = N / 10;
+    i++;
+}
+Console.WriteLine($"Сумма цифр в числе {inputNumber} равно {sum} ");
