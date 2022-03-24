2627770d5482ad2f8eee6f27bd9fcb6141925f7c
Igor <pilingator86@gmail.com>
2022-03-24 22:01:29 +0700

    Массив из 8 элементов случ числами (в виде функции)

---
+++ HW14/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+void FindArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(); 
+    }
+}
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.WriteLine(collect[7] + "]");
+}
+int[] arr = new int[8];
+
+FindArray(arr);
+PrintArray(arr);
