9a516e9224e8b04a207ce075449621460d1ef0b5
Igor <pilingator86@gmail.com>
2022-03-26 19:54:37 +0700

    Нахождение разности макс. и мин. элементов массива

---
+++ HW17/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+//элементов массива.
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(0, 100);
+    }
+    return answer;
+}
+void printArr(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(12);
+printArr(arr);
+
+int max = 0;
+int min = arr[0];
+void maximum(int[] arr)
+{
+
+    for (int i = 0; i < 12; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }  
+        
+    }
+
+}
+void minimum(int[] arr)
+{
+
+    for (int i = 0; i < 12; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }  
+        
+    }
+
+}
+minimum(arr);
+maximum(arr);
+
+Console.WriteLine($"Максимальное число массива {max}");
+Console.WriteLine($"Минимально число массива {min}");
+Console.WriteLine($"Разность максимального и минимального {max - min}");
