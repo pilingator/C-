8317ed1c5537506f8e72bffbe5a2f5d7a3bde08e
Igor <pilingator86@gmail.com>
2022-03-26 19:28:39 +0700

    Находит сумму нечетных элементов массива

---
+++ HW16/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(0, 5);
+    }
+    return answer;
+}
+void printArr(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(12);
+printArr(arr);
+int sum = 0;
+
+void sumEl(int[] arr)
+{
+
+    for (int i = 0; i < 12; i += 2)
+    {
+           
+        sum = sum + arr[i];
+    }
+
+}
+sumEl(arr);
+Console.WriteLine($"Сумма нечетных элементов массива: {sum}");
