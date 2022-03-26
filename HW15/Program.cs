7830728284914f21af7fad4d64347d255b4a11b4
Igor <pilingator86@gmail.com>
2022-03-26 18:45:23 +0700

    Считает количество четных чисел в массиве

---
+++ HW15/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(0, 1000);
+    }
+    return answer;
+}
+void printArr(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ,");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(12);
+printArr(arr);
+int count = 0;
+void chet(int[] arr)
+{
+
+
+    for (int i = 0; i < 12; i++)
+    {
+        if (arr[i] % 2 == 0)
+           
+        count++;
+    }
+
+}
+chet(arr);
+Console.WriteLine($"Четных чисел в массиве {count}");
