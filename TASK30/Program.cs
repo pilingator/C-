1755025a4ad1b8556513576f5b8daf71520aa7b7
Igor <pilingator86@gmail.com>
2022-03-27 18:06:47 +0700

    Создает копию массива, поэлемент. копированием

---
+++ TASK30/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 4: Напишите программу, которая будет создавать копию заданного массива с помощью 
+// поэлементного копирования.
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(1, 10);
+    }
+    return answer;
+}
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.Write(collect[collect.Length - 1] + "]");
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copy = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copy[i] = arr[i];
+    }
+    return copy;
+}
+
+int[] arr = RandArray(5);
+PrintArray(arr);
+int[] copy = CopyArray(arr);
+Console.WriteLine();
+PrintArray(copy);
