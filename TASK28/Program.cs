bdeacb83b9771772106b843ec87697cbd487dd68
Igor <pilingator86@gmail.com>
2022-03-27 18:04:35 +0700

    Преобразовывает десятичное число в двоичное

---
+++ TASK28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+string Dvoichnaya(int number)
+{
+    string answer = " ";
+    while (number > 0)
+    {
+        answer = number % 2 + answer;
+        number = number / 2;
+    }
+    return answer;
+}
+
+int number = Promt("Введите десятичное число => ");
+int temp = number;
+string N = Dvoichnaya(number);
+Console.WriteLine($"Число {temp} в двоичной системе => {N}");
+
