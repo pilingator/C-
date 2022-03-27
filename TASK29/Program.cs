df981904d6e88d19ccf0cedf4496e9b1b83ffe47
Igor <pilingator86@gmail.com>
2022-03-27 18:05:40 +0700

    Выводит первые N чисел Фибоначчи

---
+++ TASK29/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 3: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.Write(collect[collect.Length - 1] + "]");
+}
+
+int N = Promt("Введите число => ");
+
+int[] K = new int[N];
+K[0] = 0;
+K[1] = 1;
+for (int i = 2; i < N; i++)
+{
+    K[i] = K[i - 1] + K[i - 2];
+}
+PrintArray(K);
+
+
