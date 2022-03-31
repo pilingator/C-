91eb1f2efbcca33aa8599b3a4fe8e3d7fb7635f7
Igor <pilingator86@gmail.com>
2022-03-31 20:15:58 +0700

    Считает количество положит чисел введенных с клавиатуры

---
+++ HW18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] PromtArray(string message)
+{
+    Console.Write(message);
+    string[] arr1 = Console.ReadLine().Split();
+        int[] arr2 = new int[arr1.Length];
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        arr2[i] = int.Parse(arr1[i]);
+    }
+    return arr2;
+}
+int PositiveDigit(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] arr = PromtArray("Введите числа => ");
+int count = PositiveDigit(arr);
+Console.WriteLine($"Количество чисел больше 0 => {count}");
