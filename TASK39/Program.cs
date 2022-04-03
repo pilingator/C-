2b4e89a813f0c38ce40d0e8ed7b75ff5af139f1f
Igor <pilingator86@gmail.com>
2022-04-03 18:22:37 +0700

    Частотный словарь элементов двум. массива

---
+++ TASK39/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 3: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] CalcArray(int[,] array)
+{ 
+    int[] freq = new int[10];
+    
+    foreach (int elem in array)
+{
+    freq[elem]++;
+            
+    }
+    return freq; 
+}
+void console(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.WriteLine(Mas[pos] + "- " + pos );
+    }
+    Console.WriteLine();
+}
+
+int[,] array = FillArray(5, 5);
+PrintArray(array);
+int[] result = CalcArray(array);
+console(result);
+
+
