cd1fae5cb8e11e9c2187f16395a6371e00678d56
Igor <pilingator86@gmail.com>
2022-04-03 18:07:30 +0700

    Двумерный массив m x n, каждый элемент A = m + n

---
+++ TASK32/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(5, 4);
+PrintArray(array);
