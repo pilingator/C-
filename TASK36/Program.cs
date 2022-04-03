1ebf3c0cf69393efd7752d82d7425aabded2f868
Igor <pilingator86@gmail.com>
2022-04-03 18:17:24 +0700

    В двум массиве находит элемент с четными индексами и возвдит его в квадрат

---
+++ TASK36/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void CalcArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            if (i % 2 == 0 && j % 2 == 0)
+                array[i, j] = array[i, j] * array[i, j];
+                Console.Write($"{array[i, j]}\t");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] array = FillArray(5, 4);
+PrintArray(array);
+Console.WriteLine();
+CalcArray(array);
