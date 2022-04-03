2c551996b6004a7e6081b0321548804bb9b5ae30
Igor <pilingator86@gmail.com>
2022-04-03 17:58:11 +0700

    Находит среднее арифм. в каждом стобце двум. массива

---
+++ HW22/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-10, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+double CalcArray(int[,] array)
+{ 
+    double res = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {   
+         
+        for (int i = 0; i < array.GetLength(0); i++)
+        
+        {   
+            
+            res += array[i, j];
+            
+        }
+        Console.WriteLine($"Столбец {j} = {res / array.GetLength(0)}");
+        res = 0;
+    }
+    return res; 
+}
+
+int[,] array = FillArray(5, 4);
+PrintArray(array);
+Console.WriteLine("Среднее арифметическое значение");
+CalcArray(array);
