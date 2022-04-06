dd6c23a8b4d8399c6f4780daa9f9e1770a22a03f
Igor <pilingator86@gmail.com>
2022-04-06 21:34:37 +0700

    Удаляет строку и столбец на пересечении мин числа массива

--- TASK40/Program.cs
+++ TASK40/Program.cs
@@ -1,67 +1,55 @@
 ﻿// Задача 4: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
 // на пересечении которых расположен наименьший элемент массива.
 
-int[,] FillArray(int row, int column)
-{
-    int[,] array = new int[row, column];
-    Random rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = rnd.Next(5, 100);
-        }
-    }
-    return array;
+int[, ] FillArray(int row, int column) {
+	int[, ] array = new int[row, column];
+	Random rnd = new Random();
+	for (int i = 0; i < array.GetLength(0); i++) {
+		for (int j = 0; j < array.GetLength(1); j++) {
+			array[i, j] = rnd.Next(5, 100);
+		}
+	}
+	return array;
 }
 
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
+void PrintArray(int[, ] array) {
+	for (int i = 0; i < array.GetLength(0); i++) {
+		for (int j = 0; j < array.GetLength(1); j++) {
+			Console.Write($"{array[i, j]}\t");
+		}
+		Console.WriteLine();
+	}
 }
 
-int[,] minimum(int[,] array)
-{   int x = 0;
-    int y = 0;
-    int min = array[0, 0]; 
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-   {
-        if (min > array[i, j])
-        {
-            min = array[i, j];
-            x = i;
-            y = j;
-            
-        }  
-        
-    }
-    Console.WriteLine($"Минимальное число массива {min} находится на {x} строке в {y} столбце");
-    Console.WriteLine("Вывод преобразованной матрицы");
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        {
-            if ((i == x) || (j == y))
-                continue;
-            else
-                Console.Write(array[i, j] + " ");
-        }
-    }
-    Console.WriteLine();
-}
-    return array;
+int[, ] minimum(int[, ] array) {
+	int x = 0;
+	int y = 0;
+	int min = array[0, 0];
+	for (int i = 0; i < array.GetLength(0); i++)
+	for (int j = 0; j < array.GetLength(1); j++) {
+		if (min > array[i, j]) {
+			min = array[i, j];
+			x = i;
+			y = j;
+
+		}
+
+	}
+	Console.WriteLine($"Минимальное число массива {min} находится на {(x+1)} строке в {(y+1)} столбце");
+	Console.WriteLine($"Матрица без {(x+1)} строки и столбца {(y+1)}");
+	for (int i = 0; i < array.GetLength(0); i++) {
+		for (int j = 0; j < array.GetLength(1); j++) {
+			{
+				if ((i == x) || (j == y)) continue;
+				else Console.Write(array[i, j] + " ");
+			}
+		}
+		Console.WriteLine();
+	}
+	return array;
 }
 
-int[,] array = FillArray(5, 5);
+int[, ] array = FillArray(5, 5);
 PrintArray(array);
 minimum(array);
 
