7787a860196e56ab406e53d0c00557accda49b8a
Igor <pilingator86@gmail.com>
2022-04-10 21:37:25 +0700

    Принимает число и возвращает сумму его цифр

---
+++ TASK43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int SumN(int N)
+{
+    if(N/10 <= 0 ) return N%10 ;
+    return N%10 + SumN(N/10);
+}
+int N = Prompt("Введите число => ");
+Console.WriteLine(SumN(N));
