33a2afc5ec2c8744f39115627d7ef28e164daa6e
Igor <pilingator86@gmail.com>
2022-04-10 21:36:20 +0700

    Работа с текстом и коорд

---
+++ Lek09/Program.cs
@@ -0,0 +1,25 @@
+﻿// Вычисление координаты
+
+using System.Linq;
+
+string text = "(1,2) (3,4) (5,6) (7,8)"
+                .Replace("(","")
+                .Replace(")","")
+                ;
+    Console.WriteLine(text);
+
+var data = text.Split(" ")
+            .Select(item => item.Split(','))
+            .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+            .Where(e=> e.x % 2 == 0)
+            .Select(point => (point.x * 10, point.y + 10))
+            .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    
+    
+    Console.WriteLine(data[i]);
+    
+
+}
