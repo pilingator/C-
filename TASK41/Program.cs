6e1bfb8268a4171fcd269168126c75a9de5751b4
Igor <pilingator86@gmail.com>
2022-04-10 21:33:57 +0700

    Выдает все нат. числа от 1 до N

---
+++ TASK41/Program.cs
@@ -0,0 +1,20 @@
+﻿// адача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+void PrintN(int N,int i)
+{
+    if(i > N) return; 
+    Console.Write(i + " ");
+    PrintN(N,i+1);  
+}
+int N = Prompt("Введите число => ");
+PrintN(N,1);
