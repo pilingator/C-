728dcdf33415e49be296379d512e7271df04d4d6
Igor <pilingator86@gmail.com>
2022-04-10 21:30:10 +0700

    Вычисление функции Аккермана

---
+++ HW29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 29
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int Akker(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akker(n - 1, 1);
+    else
+      return Akker(n - 1, Akker(n, m - 1));
+}
+int A = Prompt("Введите число => ");
+int B = Prompt("Введите число => ");
+Console.WriteLine(Akker(A,B));
+
