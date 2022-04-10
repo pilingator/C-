eed22d6d64a51b331daec446c2bec7f883349d07
Igor <pilingator86@gmail.com>
2022-04-10 21:34:59 +0700

    Выдает все натуральные числа от M до N

---
+++ TASK42/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+void PrintN(int N,int i)
+{
+    if(i > N) return; 
+    Console.Write(i + " ");
+    PrintN(N,i+1);  
+}
+int M = Prompt("Введите число => ");
+int N = Prompt("Введите число => ");
+PrintN(N,M);
