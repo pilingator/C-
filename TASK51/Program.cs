96a56890d120abe2d86642dc9ab581fd0e84d7ff
pilingator <pilingator86@gmail.com>
2022-04-20 22:45:57 +0700

    Разворачивает число с помощью рекурсии

---
+++ TASK51/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Разверните число при помощи рекурсии: N = 1234 -> "4321"
+int ReversNumber(int N, int k = 0)
+{
+if(N == 0)
+{
+    Console.WriteLine(k);
+    return k;
+}
+
+return ReversNumber(N/10, k * 10 + N % 10);
+}
+Console.WriteLine("Введите N: ");
+var n = int.Parse(Console.ReadLine());
+ReversNumber(n);
