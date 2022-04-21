32e7f474efb1652f79bff98dfbc3c1599dc6bfc2
pilingator <pilingator86@gmail.com>
2022-04-21 22:26:01 +0700

    Определяет, является ли число степенью 2

---
+++ HW30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+void Stepen2(int N)
+{
+    
+    if (N / 2 == 0)
+    {
+        Console.WriteLine("Является степенью числа 2");
+    return;
+    }
+    if (N % 2 == 0)
+    {
+       
+        Stepen2(N/2);
+    }
+    else Console.WriteLine("Не является степенью числа 2");
+}
+
+Console.WriteLine("Введите N: ");
+var n = int.Parse(Console.ReadLine());
+Stepen2(n);
