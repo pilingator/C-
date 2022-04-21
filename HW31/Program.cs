8bc419adf0dc3189c707f25e15ffdc0c40246d0d
pilingator <pilingator86@gmail.com>
2022-04-21 22:26:47 +0700

    Проверка на прстое число

---
+++ HW31/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+void SimpleDigit(int N, int K)
+{
+    
+    if (N / K == 1)
+    {
+        Console.WriteLine("Является простым числом");
+    return;
+    }
+    if (N % K != 0)
+    {
+       
+        SimpleDigit(N, K + 1);
+    }
+    else Console.WriteLine("Не является простым числом");
+}
+
+Console.WriteLine("Введите N: ");
+var n = int.Parse(Console.ReadLine());
+SimpleDigit(n, 2);
